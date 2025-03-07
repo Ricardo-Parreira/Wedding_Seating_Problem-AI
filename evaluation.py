9e7f34fb0a855e971aab04162c7004c015656e39
Sara Cortez <131759998+SarinhaCortez@users.noreply.github.com>
2025-03-07 09:30:27 +0000

    added notebook

--- evaluation.py
+++ evaluation.py
@@ -3,11 +3,10 @@ test_arragenment = [[0,2], [1,3]]
 test_matrix = [[0,-10, 100, -100],
               [10, 0, 0, 100],
               [0,0,0,0],
-              [-100, 100, -50, 0]]
+              [-100, 100, -50, 0]]  #ABCDABCD
 
 def evaluate_table(tables, matrix):
     score = 0
-
     #nao me parece que isto seja a forma mais eficaz mas é só para termos uma base
     for table in tables:
         for guest in table:
